a748c98f7fd244eff5a514aef8f63ed7f4a827fb
ashikurriyal <riyal.ashikur@gmail.com>
2024-05-04 16:07:52 +0600

    Updated the banner section

--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -1,9 +1,10 @@
+import Banner from "../Banner/Banner";
 
 
 const Home = () => {
     return (
         <div>
-            <h2 className="5xl">This is Home</h2>
+            <Banner></Banner>
         </div>
     );
 };
