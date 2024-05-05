f35fca280e9742fa01b960f577b8ee9a4ece1899
ashikurriyal <riyal.ashikur@gmail.com>
2024-05-05 19:30:42 +0600

    Initialized the login page and firebase authentication setup

--- src/Pages/Home/Services/Services.jsx
+++ src/Pages/Home/Services/Services.jsx
@@ -14,12 +14,12 @@ const Services = () => {
     return (
         <div className="mt-28">
             <div className="text-center space-y-5">
-                <h3 className="h3 text-xl font-bold text-orange-600">Service</h3>
-                <h3 className="h3 text-3xl font-bold">Our Service Area</h3>
+                <h3 className="text-xl font-bold text-orange-600">Service</h3>
+                <h3 className="text-3xl font-bold">Our Service Area</h3>
                 <p>the majority have suffered alteration in some form, by injected humour, or randomised <br />words which donot look even slightly believable. </p>
             </div>
 
-            <div className="grid gird-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 lg:mt-12">
+            <div className="grid gird-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10 lg:mt-12 mb-12">
                 {
                     services.map(service => <ServiceCard key={service._id} service={service}></ServiceCard>)
                 }
